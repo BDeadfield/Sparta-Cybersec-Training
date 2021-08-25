efbf4d70dce86de04321beb85d1697b34751e11d
Bradley <bradleydeadfield@gmail.com>
2021-08-25 20:42:24 +0100

    Added comments to processes script.

--- Basic Bash Scripting/Process Log/processes.sh
+++ Basic Bash Scripting/Process Log/processes.sh
@@ -1,10 +1,17 @@
+# Create 'processes' directory and clear console log
+
 mkdir processes
-clear
+
+# Continuously loop (until the user exits)
 
 while true
 do
 
+# Asign a file path based on time / date
+
    DIRECTORY="processes/"$(date +"%F-%T").log
+
+# Create a log file at said file path and fill it with the system's currently running processes every 30 seconds
 
    touch $DIRECTORY
    ps -e > $DIRECTORY
