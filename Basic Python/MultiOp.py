440634aca17f76660da00c89afef1f392b0ecd3d
BDeadfield <bradleydeadfield@gmail.com>
2021-09-07 14:05:56 +0100

    Update MultiOp.py

--- Basic Python/MultiOp.py
+++ Basic Python/MultiOp.py
@@ -1,13 +1,21 @@
+# Read user input and store as an integer
+
 firstNumber = int(input("What's the first number?"))
 secondNumber = int(input("What's the second number?"))
+
+# Perform maths and store as variable
 
 sum = firstNumber + secondNumber
 sub = firstNumber - secondNumber
 mul = firstNumber * secondNumber
 
+# Print equation and sum
+
 print(firstNumber, '+', secondNumber, '=', sum)
 print(firstNumber, '-', secondNumber, '=', sub)
 print(firstNumber, '*', secondNumber, '=', mul)
+
+# Calculate / print division and sum if user stored numbers are greater than zero
 
 if(firstNumber and secondNumber > 0):
     div = firstNumber / secondNumber
